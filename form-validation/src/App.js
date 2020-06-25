37d0215661b4e3dc747cf007576f0a67b2ec97ce
it18016444 <it18016444@my.sliit.lk>
2020-06-24 23:25:10 -0700

    commit4

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -6,11 +6,61 @@ class App extends Component{
 
   render(){
 
-  return (
-    <div className="App">
-     hello world
+  return <div className="wrapper">
+    <div className="form-wrapper">
+    <h1>Create Account</h1>
+    <form onSubmit={this.handleSubmit} noValidate>
+      <div className="firstName">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          className=""
+          placeholder="First Name"
+          name="firstName"
+          noValidate
+          onChange={this.handleChange}
+          />
+      </div>
+
+      <div className="LastName">
+      <label htmlFor="LastName">Last Name</label>
+      <input
+        type="text"
+        className=""
+        placeholder="Last Name"
+        name="LastName"
+        noValidate
+        onChange={this.handleChange}
+        />
     </div>
-  );
+
+    <div className="email">
+    <label htmlFor="email">email</label>
+    <input
+      type="text"
+      className=""
+      placeholder="email"
+      name="email"
+      noValidate
+      onChange={this.handleChange}
+      />
+  </div>
+
+  <div className="password">
+  <label htmlFor="password">password</label>
+  <input
+    type="text"
+    className=""
+    placeholder="password"
+    name="password"
+    noValidate
+    onChange={this.handleChange}
+    />
+</div>
+    </form>
+    </div>
+  </div>;
+ 
 }
 }
 export default App;
